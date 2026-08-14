d30c1c13533a0d2b9b941dc208c453fce399a5ae
Yasmin Silva da Conceição <yasmisilvaconc@gmail.com>
2026-08-14 18:09:51 -0300

    useContext 2

---
+++ useContext2/src/GlobalContext.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+
+export const GlobalContext = React.createContext()
+
+export const GlobalStorage = ({ children }) => {
+
+  const [dados, setDados] = React.useState(null)
+
+  React.useEffect(() => {
+    fetch('https://ranekapi.origamid.dev/json/api/produto')
+    .then((response) => response.json())
+    .then((json) => setDados(json))
+  },[])
+
+  const limparDados = () => {
+    setDados(null)
+  }
+
+  return (
+    <GlobalContext.Provider value={{ dados, limparDados }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
