415258517ad35da30b5b0f98300d242c4ece7eef
Yasmin Silva da Conceição <yasmisilvaconc@gmail.com>
2026-08-26 16:50:39 -0300

    aplication

--- input/src/App.jsx
+++ input/src/App.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
 
 const App = () => {
-  return (
-    <div>
-      Hello
-    </div>
-  )
+	// const [nome, setNome] = React.useState('')
+  // const [email, setEmail] = React.useState('')
+  const [form, setForm] = React.useState({
+    nome: '',
+    email: ''
+  })
+
+  // const handleSubmit = (event) => {
+  //   event.preventDefault()
+  //   console.log(event)
+    
+  // }
+
+  const handleChange = ({target}) => {
+    const [id, value] = target
+    setForm({ ...form, [id]: value })
+
+  }
+
+	return (
+		<form onSubmit={handleSubmit}>
+      <label htmlFor="nome">Nome</label>
+			<input 
+        id='nome'
+        type='text' 
+        value={form.nome} 
+        name='nome'
+        onChange={handleChange}
+      />
+
+      <label htmlFor="email">Email</label>
+			<input 
+        id='email'
+        type='email' 
+        value={form.email} 
+        name='email'
+        onChange={handleChange}
+      />
+      
+
+      <button>Enviar</button>
+		</form>
+	)
 }
 
 export default App
