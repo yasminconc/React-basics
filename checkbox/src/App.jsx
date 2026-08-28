a1d2c346316c890d5aca4f54daff68d28af2adc0
Yasmin Silva da Conceição <yasmisilvaconc@gmail.com>
2026-08-28 17:13:09 -0300

    checkbox

---
+++ checkbox/src/App.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const App = () => {
+  const [cores, setCores] = React.useState([])
+
+  const handleChange = ({target}) => {
+    if(target.checked) {
+      setCores([...cores, target.value])
+    }else {
+      setCores(cores.filter((cor) => cor !== target.value))
+    }
+  }
+
+  return (
+    <div>
+      <label>
+        <input 
+          type="checkbox"
+          value='azul' 
+          checked={cores.includes('azul')}
+          onChange={handleChange}
+        />
+        Azul
+      </label>
+      
+      <label>
+        <input 
+          type="checkbox"
+          value='vermelho' 
+          checked={cores.includes('vermelho')}
+          onChange={handleChange}
+        />
+        Vermelho
+      </label>
+    </div>
+  )
+}
+
+export default App
